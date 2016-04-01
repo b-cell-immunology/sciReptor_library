aa5767751e293161c74ad058e3f5bca56e0027b3
Christian Busse <christian.busse@dkfz-heidelberg.de>
2016-04-02 00:08:54 +0200

    Sanity checks for tag generation script
    Add test for meaningful tag structure. Add cross-checks between
    tag lists and matrix batch list. Make the working directory
    configurable via command line. Rename output file from ".csv"
    to ".tsv".

--- load_lib_tables.sql
+++ load_lib_tables.sql
@@ -1,11 +1,11 @@
 
 -- SET @plate_file=(SELECT concat(@abs_path, '/library/plate_layout/plate_layout_library.csv'));
 -- SET @species_file=(SELECT concat(@abs_path, '/library/species/species_library.csv'));
--- SET @tag_file=(SELECT concat(@abs_path, '/tags/tag_library.csv'));
+-- SET @tag_file=(SELECT concat(@abs_path, '/tags/tag_library.tsv'));
 
 LOAD DATA LOCAL INFILE './plate_layout/plate_layout_library.csv' INTO TABLE plate_layout_library IGNORE 1 LINES;
 
 LOAD DATA LOCAL INFILE './species/species_library.csv' INTO TABLE species_library IGNORE 1 LINES;
 
-LOAD DATA LOCAL INFILE './tags/tag_library.csv' INTO TABLE tags_library;
+LOAD DATA LOCAL INFILE './tags/tag_library.tsv' INTO TABLE tags_library;
 
--- tags/process_tags.pl
+++ tags/process_tags.pl
@@ -4,58 +4,101 @@
 #
 # USE FOLLOWING COMMAND TO UPLOAD TO DATABASE
 # note: use from command line with --local-infile=1 option, otherwise the load data local... is blocked
-# load data local infile 'tag_library.csv' into table tags_library";
+# load data local infile 'tag_library.tsv' into table tags_library";
 #
 use strict;
 use warnings;
 use File::Find;
-
-my $option_directory = ".";
-my $re_filename_tags = qr /.*tags_[0-9.ki]+_([0-9]{2,3})x([0-9]{2,3})_matrix\.tsv$/;
+use Getopt::Long;
 
 my %hash_matrix_batch;
+my $re_filename_tags = qr/.*tags_[0-9.ki]+_([0-9]{2,3})x([0-9]{2,3})_matrix\.tsv$/;
+my $re_tag_sanity = qr/^[ACGT]{16}$/;
 
-open my $fh_batches, "<", "matrix_batches.tsv" or die "Could not open matrix_batches.tsv";
-foreach (<$fh_batches>) {
+my $filename_matrix_batch = "matrix_batches.tsv";
+my $filename_output = "tag_library.tsv";
+
+my $option_show_help = '';
+my $option_directory = '.';
+
+GetOptions (
+	'help' => \$option_show_help,
+	'directory=s' => \$option_directory
+);
+
+if ($option_show_help) {
+	print "Usage: process_tags.pl [--directory=PATH]\n";
+	print " --directory  : use directory PATH as working directory (default: current directory)\n";
+	exit;
+}
+
+$option_directory =~ s/\/$//;
+
+open my $fh_batches, "<", $option_directory . "/" . $filename_matrix_batch or die "Could not open input file " . $filename_matrix_batch;
+while (<$fh_batches>) {
+	next if ($. == 1);
 	chomp $_;
 	(my $matrix, my $batch) = split /\t/, $_;
-	push @{$hash_matrix_batch{$matrix}}, $batch;
+	push @{$hash_matrix_batch{$matrix}{batch}}, $batch;
+	$hash_matrix_batch{$matrix}{processed} = 0;
 }
 close $fh_batches;
 
-open my $fh_output, ">", "tag_library.csv" or die "Could not open tag_library.csv";
+open my $fh_output, ">", $option_directory . "/" . $filename_output or die "Could not open output file " . $filename_output;
 
 my @list_files_tags = ();
 {
-    sub sub_find_regex_files {
-        m/$re_filename_tags/ &&
-        -f $_ &&
-        push @list_files_tags, $File::Find::name;
-    }
-    find(\&sub_find_regex_files,("$option_directory"))
+	sub sub_find_regex_files {
+		m/$re_filename_tags/ &&
+		-f $_ &&
+		push @list_files_tags, $File::Find::name;
+	}
+	find(\&sub_find_regex_files,("$option_directory"))
 }
 
 foreach my $filename_current ( sort @list_files_tags ) {
-
 	my $matrix = $filename_current;
 	$matrix =~ s/$re_filename_tags/$1_$2/;
-	open my $fh_current, "<", $filename_current;
-	while (<$fh_current>) {
-		chomp $_;
-		next if ($_ =~ m/number/);  # skip header line
+	if ( defined $hash_matrix_batch{$matrix} ) {
+		printf "[process_tags.pl][INFO] Processing matrix %s (used by %i batches).\n", $matrix, scalar @{$hash_matrix_batch{$matrix}{batch}};
+		open my $fh_current, "<", $filename_current;
+		while (<$fh_current>) {
+			chomp $_;
+			next if ($_ =~ m/number/);  # skip header line
 
-		(my $id, my $col_tag, my $row_tag) = split /\t/, $_;
+			(my $id, my $col_tag, my $row_tag) = split /\t/, $_;
 
-		foreach my $batch ( @{$hash_matrix_batch{$matrix}} ) {
-			if ($row_tag) {
-				printf $fh_output "\tR%03d\t%s\t%s\t%s\n", $id, $row_tag, $matrix, $batch;
-			}
-			if ($col_tag) {
-				printf $fh_output "\tC%03d\t%s\t%s\t%s\n", $id, $col_tag, $matrix, $batch;
+			foreach my $batch ( @{$hash_matrix_batch{$matrix}{batch}} ) {
+				if ($row_tag) {
+					if ( $row_tag =~ m/$re_tag_sanity/ ) {
+						printf $fh_output "\tR%03d\t%s\t%s\t%s\n", $id, $row_tag, $matrix, $batch;
+					} else {
+						printf "[process_tags.pl][ERROR] Detected invalid row tag \"%s\" in matrix %s!\n", $row_tag, $matrix;
+						exit 1;
+					}
+				}
+				if ($col_tag) {
+					if ( $col_tag =~ m/$re_tag_sanity/ ) {
+						printf $fh_output "\tC%03d\t%s\t%s\t%s\n", $id, $col_tag, $matrix, $batch;
+					} else {
+						printf "[process_tags.pl][ERROR] Detected invalid column tag \"%s\" in matrix %s!\n", $col_tag, $matrix;
+						exit 1;
+					}
+				}
 			}
 		}
+		close $fh_current;
+		$hash_matrix_batch{$matrix}{processed} = 1;
+	} else {
+		printf "[process_tags.pl][WARNING] Matrix size %s not defined in \"%s\". Tags file \"%s\" will be skipped!\n",
+			$matrix, $filename_matrix_batch, $filename_current
+		;
 	}
-	close $fh_current;
 }
-
 close $fh_output;
+
+foreach (keys %hash_matrix_batch) {
+	if (! $hash_matrix_batch{$_}{processed}) {
+		printf "[process_tags.pl][WARNING] No tags were processed for matrix %s (used by %i batches).\n", $_, scalar @{$hash_matrix_batch{$_}{batch}};
+	}
+}
